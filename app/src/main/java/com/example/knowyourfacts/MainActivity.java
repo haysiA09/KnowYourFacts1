1e70b80a0273e0f456901d6e392abc457f40a9c5
16022877 <16022877@myrp.edu.sg>
2019-07-20 00:59:17 +0800

    bvjvjh

--- app/src/main/java/com/example/knowyourfacts/MainActivity.java
+++ app/src/main/java/com/example/knowyourfacts/MainActivity.java
@@ -5,22 +5,30 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager vPager;
+    ArrayList <Fragment> al;
+    MyFragmentPagerAdapter adapter;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<Fragment> al;
-        MyFragmentPagerAdapter adapter;
-        ViewPager vPager;
+
 
 
 
         vPager = findViewById(R.id.viewpager1);
+
 
         FragmentManager fm = getSupportFragmentManager();
 
@@ -32,5 +40,48 @@ public class MainActivity extends AppCompatActivity {
 
         vPager.setAdapter(adapter);
 
+
+
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        vPager = findViewById(R.id.viewpager1);
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.action_next:
+                // do your code
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1) {
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+                    return true;
+            case R.id.action_previous:
+                // do your code
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+
+                return true;
+            case R.id.action_random:
+                // do your code
+
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
+
+
+    }
 }
